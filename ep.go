839ef2333b50349ce1c0f6eb75a64132aa9fba40
tarvo <arikastarvo@gmail.com>
2021-03-31 15:49:13 +0300

    add event_type_path field to output (full path that reflects the actual matched event type nesting)

--- ep.go
+++ ep.go
@@ -160,6 +160,7 @@ func parseLine(result map[string]string, parent string) {
 		// if we have a match (captured values), then gather results, optionally parse child patterns and finally break look 	
 		if len(match) > 0 {
 			result["event_type"] = pat.Name
+			result["event_type_path"] += "/" + pat.Name
 
 			// delete source field if not stated otherwise
 			value := result[field]
